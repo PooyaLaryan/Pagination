4fab4d856a7792bf71c2d8218ea374c45b5b93d8
Lariyan.Pouya <Lariyan.Pouya@HIG001P4987>
2024-07-27 17:32:51 +0330

    Add project files.

---
+++ Samples/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Samples;
+
+public static class Extensions
+{
+    public static IEnumerable<T> Pagination<T>(this IEnumerable<T> data, int pageIndex, int pageSize) =>
+        data.Skip(pageIndex * pageSize).Take(pageSize);
+
+    public static IEnumerable<T> FirstPage<T>(this IEnumerable<T> data, int PageSize) =>
+        data.Take(PageSize);
+
+    public static IEnumerable<T> LastPage<T>(this IEnumerable<T> data, int pageSize) =>
+        data.Skip((data.Count() / pageSize - 1) * pageSize).Take(pageSize);
+
+    public static int PageCount<T>(this IEnumerable<T> data, int pageSize)
+    {
+        var totalData = data.Count();
+        return totalData / pageSize + (totalData % pageSize > 0 ? 1 : 0);
+    }
+
+}
---
+++ Samples/Program.cs
@@ -0,0 +1,21 @@
+﻿using Samples;
+
+const int PAGE_SIZE = 25;
+const int PAGE_INDEX = 5;
+
+var data = Enumerable.Range(0, 1005).Select(i => i + 1).ToList();
+
+var first = data.FirstPage(PAGE_SIZE).ToList();
+Console.WriteLine("--------- First");
+first.ForEach(i => Console.WriteLine(i));
+
+var last = data.LastPage(PAGE_SIZE).ToList();
+Console.WriteLine("--------- Last");
+last.ForEach(i => Console.WriteLine(i));
+
+var page5 = data.Pagination(PAGE_INDEX, PAGE_SIZE).ToList();
+Console.WriteLine("--------- page 5");
+page5.ForEach(i => Console.WriteLine(i));
+
+var countOfPages = data.PageCount(PAGE_SIZE);
+Console.WriteLine($"Count of pages : {countOfPages}");
